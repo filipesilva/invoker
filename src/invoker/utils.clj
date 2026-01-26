c2a08fe03221a99a31867463db6e7cb99cd39237
Filipe Silva <filipematossilva@gmail.com>
2026-01-26 21:38:31 +0000

    fix: ext-type always takes precedence over accept

--- src/invoker/utils.clj
+++ src/invoker/utils.clj
@@ -221,9 +221,7 @@
         pre-render          (merge {} (-> var meta :invoker/pre-render))
         ext-type            (-> ext extensions mime-type-str)
         content-type        (or content-type ext-type)
-        accept              (if (and ext-type (= accept "*/*"))
-                              ext-type
-                              (or accept ext-type))
+        accept              (or ext-type accept)
         body-content-type   (or (not content-type)
                                 (not body)
                                 (mime-match content-type parse)
